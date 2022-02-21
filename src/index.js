8b99a3369cf8e80975f573c954b78b87ee38b703
soferdani <soferdani@gmail.com>
2022-02-21 14:12:44 +0200

    output manegmant done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import _ from 'lodash';
-import "./style.css";
-import Icon from './icon.png';
+import printMe from './print';
+
+
 
 function component() {
     const element = document.createElement('div');
-
+    const btn = document.createElement('button');
+  
   // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    btn.innerHTML = 'click me and check the console!';
+    btn.onclick = printMe;
 
-   // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
+    element.appendChild(btn);
   
     return element;
 }
